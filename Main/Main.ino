06d09214ed35b75c96e0e4694708210d16af275e
Liyanage Malitha <e15299@ce.pdn.ac.lk>
2020-01-26 18:30:49 +0530

    Included libraries and vars

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,7 +1,29 @@
-void setup() {
+#include <SoftwareSerial.h>
+#include <ArduinoJson.h>
+
+SoftwareSerial s(14,12);
+int data;
+
+int lightVal;
+int nutriVal;
+int waterVal;
+float phVal;
+float tempVal;
+float humidityVal;
+
+int serialRecivalIndicatorPin = 5;
+
+
+void setup() 
+{
+  pinMode(serialRecivalIndicatorPin, OUTPUT);
+  s.begin(9600);
+  Serial.begin(9600);
+  while (!Serial) continue;
   
 }
 
-void loop() {
-  
+void loop() 
+{
+  SerialRecieval();
 }
