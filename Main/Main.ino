2486820481b6ebec5ae317b97c890d074e428ac8
Liyanage Malitha <e15299@ce.pdn.ac.lk>
2020-01-26 20:02:10 +0530

    Firebase set data added

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,13 +1,18 @@
 #include <SoftwareSerial.h>
 #include <ArduinoJson.h>
-
+#include "FirebaseESP8266.h"
 #include <ESP8266WiFi.h>
 
 #define WIFI_SSID "Malitha"
 #define WIFI_PASSWORD "racooncity"
 
+#define FIREBASE_HOST "test1-64187.firebaseio.com"
+#define FIREBASE_AUTH "RARZjya5M15q0zdkzK5E60kOTjJl4Dtin5kZUXGH"
+
 #include <NTPClient.h>
 #include <WiFiUdp.h>
+
+
 
 SoftwareSerial s(14,12);
 int data;
@@ -28,6 +33,8 @@ NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
 String date [3];
 String Time [2];
 
+FirebaseData firebaseData;
+
 void setup() 
 {
   pinMode(serialRecivalIndicatorPin, OUTPUT);
@@ -38,6 +45,14 @@ void setup()
   ConnectToWifi();
 
   timeClient.begin();
+
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.reconnectWiFi(true);
+
+  String storePath = "/ESP8266_Test";
+  String jsonStr;
+
+  String retrievePath = "/ESP8266_Test";
   
 }
 
