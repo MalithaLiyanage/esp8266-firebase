216b24e40de5ddfdf7918006845df4bff6c1f991
Liyanage Malitha <e15299@ce.pdn.ac.lk>
2020-01-30 13:55:43 +0530

    Actuators done

--- Main/Actuator.ino
+++ Main/Actuator.ino
@@ -1,1 +1,23 @@
+void startLights()
+{
+  digitalWrite(4, LOW);  // D2 pin
+  
+}
 
+void endLights()
+{
+  digitalWrite(4, HIGH);  // D2 pin
+}
+
+void openNutrientValve()
+{
+  digitalWrite(0, LOW); // D3 pin
+  delay(10000);
+  digitalWrite(0, HIGH);
+  
+}
+
+void closeNutrientValve()
+{
+  digitalWrite(0, LOW);
+}
